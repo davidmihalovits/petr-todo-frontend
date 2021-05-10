f20b3d2671c7406f8193f3570b94214959ba1f37
Dávid Lajos Mihalovits <dmihalovits@gmail.com>
2021-05-10 13:45:49 +0200

    new

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -23,16 +23,38 @@ const Home = () => {
         <div className="home">
             <h1 className="home-title">Welcome</h1>
             <p className="home-description">
-                This a simple PETR-stack todo app. 
+                This a simple PETR-stack todo app.
             </p>
             <p className="home-description">
                 P stands for PostgreSQL, E for Express, T for TypeScript and R
-                for React. I implemented server-side authentication, basic CRUD functionalities, global
-                state management with Redux and so on.
+                for React. I implemented server-side authentication, basic CRUD
+                functionalities, global state management with Redux and so on.
             </p>
             <p className="home-description">
                 Please log in to use this app. Everything is saved in the cloud
                 so your notes are synchronized across devices.
+            </p>
+            <p className="home-paragraph">
+                Frontend code:{" "}
+                <a
+                    href="https://github.com/davidmihalovits/petr-todo-frontend"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="home-link"
+                >
+                    https://github.com/davidmihalovits/petr-todo-frontend
+                </a>
+            </p>
+            <p className="home-paragraph">
+                Backend code:{" "}
+                <a
+                    href="https://github.com/davidmihalovits/petr-todo-backend"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="home-link"
+                >
+                    https://github.com/davidmihalovits/petr-todo-backend
+                </a>
             </p>
         </div>
     );
